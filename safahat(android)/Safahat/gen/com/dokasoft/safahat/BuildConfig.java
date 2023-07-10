42011d22e3854694e9eaaebf51c294d15fea31f2
mdogubasalan <dokayazilim@gmail.com>
2023-07-10 16:26:01 +0300

    Android ile Safahat Uygulaması (2012)
    Android öğrendiğimde yapmış olduğum ilk uygulama çalışması.

---
+++ safahat(android)/Safahat/gen/com/dokasoft/safahat/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dokasoft.safahat;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ safahat(android)/Safahat/src/com/dokasoft/safahat/kitap9.java
@@ -0,0 +1,15 @@
+package com.dokasoft.safahat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class kitap9 extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		 setContentView(R.layout.kitap9);
+	}
+
+}
